129653d9b77bdaeece49090938de9a016863460d
xmaxinex4 <maxinegerhard@gmail.com>
2021-03-21 23:04:29 -0400

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,37 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb"],
-  parser: "@typescript-eslint/parser",
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    // note you must disable the base rule as it can report incorrect errors
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React, { useCallback, useState } from "react";
-import "./App.css";
+import React, { useCallback, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [response, setResponse] = useState("No response yet");
+  const [response, setResponse] = useState('No response yet');
 
   const getApi = (endpoint: any) => {
     fetch(
-      `https://game-board-group-heroku-api.herokuapp.com/api/${endpoint}`
-    ).then(function (r) {
+      `https://game-board-group-heroku-api.herokuapp.com/api/${endpoint}`,
+    ).then((r) => {
       r.text().then((value) => {
         setResponse(value);
       });
     });
   };
 
-  const checkStatus = useCallback(() => getApi(""), []);
+  const checkStatus = useCallback(() => getApi(''), []);
 
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.jsx</code> and save to reload.
+          Edit
+          <code>
+            src/App.jsx
+          </code>
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -31,8 +35,11 @@ function App() {
           Learn React
         </a>
       </header>
-      <button onClick={checkStatus}>Check API status</button>
-      <div>Status: {response}</div>
+      <button type="button" onClick={checkStatus}>Check API status</button>
+      <div>
+        Status:
+        {response}
+      </div>
     </div>
   );
 }
