66ab2836499c70d51807f9e06ba1753c2152c555
xmaxinex4 <maxinegerhard@gmail.com>
2021-10-08 23:15:13 -0400

    cleaning up add group view

--- client/src/modules/group/no-active-group.tsx
+++ client/src/modules/group/no-active-group.tsx
@@ -1,18 +1,40 @@
 import React from "react";
 
-import { Typography, Grid } from "@mui/material";
+import Icon from "@mdi/react";
+import { mdiPlus } from "@mdi/js";
+import { makeStyles } from "@mui/styles";
 
-import { AddGroupButton } from "./add-group-button";
+import {
+  Button,
+  Typography,
+  Grid,
+  Container,
+} from "@mui/material";
+
+const useStyles = makeStyles({
+  gridContainerPadding: {
+    paddingTop: 150,
+  },
+});
 
 export function NoActiveGroup(): React.ReactElement {
+  const { gridContainerPadding } = useStyles();
+
   return (
-    <Grid container direction="column">
-      <Grid item>
-        <Typography>It looks like you do not have any groups. Add a new group to get started.</Typography>
+    <Container maxWidth="sm">
+      <Grid container className={gridContainerPadding} justifyContent="center" alignItems="center" direction="column" spacing={4}>
+        <Grid container item direction="column" alignItems="center" spacing={1}>
+          <Grid item>
+            <Typography>It looks like you don&apos;t have any groups.</Typography>
+          </Grid>
+          <Grid item>
+            <Typography>Let&apos;s add a new group to get started!</Typography>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Button variant="outlined" startIcon={<Icon path={mdiPlus} size={0.5} />}>Add Group</Button>
+        </Grid>
       </Grid>
-      <Grid item>
-        <AddGroupButton variant="text" />
-      </Grid>
-    </Grid>
+    </Container>
   );
 }
