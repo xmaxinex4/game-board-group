0b83c067838f2260d338b309bd0cbf572c3abe11
xmaxinex4 <maxinegerhard@gmail.com>
2021-10-09 23:50:35 -0400

    fixing error

--- client/src/modules/user/user-circle-list-display.tsx
+++ client/src/modules/user/user-circle-list-display.tsx
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 }));
 
-export function Library(props: UserCircleListDisplayProps): React.ReactElement {
+export function UserCircleListDisplay(props: UserCircleListDisplayProps): React.ReactElement {
   const { users, onEditUsers } = props;
   const { meeple } = useStyles({});
 
