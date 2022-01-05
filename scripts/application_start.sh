4c03bdcaf7bbf5d330cafc85557ed34c5f46c33e
xmaxinex4 <maxinegerhard@gmail.com>
2022-01-05 01:50:15 -0500

    fixing user

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,11 +2,11 @@
 
 # give permission to the files inside /secure_docs directory
 
-sudo chmod -R 777 /home/ubuntu/gbg_app
+sudo chmod -R 777 /home/ec2-user/gbg_app
 
 # navigate into current working directory
 
-cd /home/ubuntu/gbg_app
+cd /home/ec2-user/gbg_app
 
 npm run build 
 npm install
