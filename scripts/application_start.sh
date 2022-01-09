337058056b501c1f75b68cfc86cd9d37d61f1878
xmaxinex4 <maxinegerhard@gmail.com>
2022-01-09 15:16:40 -0500

    env set file test

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -16,15 +16,15 @@ sudo /usr/bin/npm run build
 sudo yum update -y
 sudo yum install -y jq
 
-ACCESS_CONTROL_ALLOW_ORIGIN=$(aws secretsmanager get-secret-value --region us-west-2 --secret-id ACCESS_CONTROL_ALLOW_ORIGIN --query SecretString --output text | jq -r .ACCESS_CONTROL_ALLOW_ORIGIN)
-APIBASEURL=$(aws secretsmanager get-secret-value --region us-west-2 --secret-id APIBASEURL --query SecretString --output text | jq -r .APIBASEURL)
-BASEURL=$(aws secretsmanager get-secret-value --region us-west-2 --secret-id BASEURL --query SecretString --output text | jq -r .BASEURL)
-APP_SECRET=$(aws secretsmanager get-secret-value --region us-west-2 --secret-id APP_SECRET --query SecretString --output text | jq -r .APP_SECRET)
-DATABASE_URL=$(aws secretsmanager get-secret-value --region us-west-2 --secret-id DATABASE_URL --query SecretString --output text | jq -r .DATABASE_URL)
-SENDGRID_API_KEY=$(aws secretsmanager get-secret-value --region us-west-2 --secret-id SENDGRID_API_KEY --query SecretString --output text | jq -r .SENDGRID_API_KEY)
+rm -f -- .env
+> .env
 
-
-echo $myValue > /var/log/echoSecret.txt
+echo 'ACCESS_CONTROL_ALLOW_ORIGIN=$(aws secretsmanager get-secret-value --region us-west-2 --secret-id ACCESS_CONTROL_ALLOW_ORIGIN --query SecretString --output text | jq -r .ACCESS_CONTROL_ALLOW_ORIGIN)' >> .env
+echo 'APIBASEURL=$(aws secretsmanager get-secret-value --region us-west-2 --secret-id APIBASEURL --query SecretString --output text | jq -r .APIBASEURL)' >> .env
+echo 'BASEURL=$(aws secretsmanager get-secret-value --region us-west-2 --secret-id BASEURL --query SecretString --output text | jq -r .BASEURL)' >> .env
+echo 'APP_SECRET=$(aws secretsmanager get-secret-value --region us-west-2 --secret-id APP_SECRET --query SecretString --output text | jq -r .APP_SECRET)' >> .env
+echo 'DATABASE_URL=$(aws secretsmanager get-secret-value --region us-west-2 --secret-id DATABASE_URL --query SecretString --output text | jq -r .DATABASE_URL)' >> .env
+echo 'SENDGRID_API_KEY=$(aws secretsmanager get-secret-value --region us-west-2 --secret-id SENDGRID_API_KEY --query SecretString --output text | jq -r .SENDGRID_API_KEY)' >> .env
 
 # start our node app in the background using pm2
 pm2 describe app > /dev/null
